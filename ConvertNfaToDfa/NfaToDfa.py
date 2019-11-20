c91617a1331a302feef92d0401d1eeb1c1aceacd
Yan-Daojiang <1373836603@qq.com>
2019-11-20 20:20:02 +0800

    m实现ε-closure函数和move函数

--- ConvertNfaToDfa/NfaToDfa.py
+++ ConvertNfaToDfa/NfaToDfa.py
@@ -13,63 +13,45 @@ def readNfa(inputFile):
     Z = set(M["Z"])
     return  K, E, f, S, Z
 
-
-def set_cache(E):
-    """设置一个字典cache进行记录，字典的键是字母表和epsilon弧"""
-    cache = {}
-    for i in E:
-        cache[i] = {}
-    cache['#'] = {}
-    return cache
-
-
-def epsilon_closure(f, cache, I):
-    """
-    状态集合I的epsilon闭包:
-    定义为状态集I中的任何状态S经过任意条epsilon弧能到达的状态
-    """
-    return closure(f, cache["#"], I, '#')
-
-def move(f, cache, I, arc):
-    return closure(f, cache[arc], I, arc)
-
-def closure(f, cache, I, arc):
-    """闭包的实现，起始的状态为I,边为arc"""
-    res = set()
+def epsilon_closure(f,I):
+    """求状态集合I:ε-clouure(I)"""
+    I = set(I)
+    # 设置每个状态的标志
+    closure_flag = dict()
     for i in I:
-        if not i in cache:
-            cache[i] = set()
-            # 判断转换弧为epsilon时
-            if arc == '#':
-                cache[i] = set([i])   #将本身状态加入其中
-            # 实现 move
-            if i in f:
-                if arc in f[i]:
-                    # 如果为epsilon进行递归,确定经过任意条epsilon弧
-                    if arc == '#':
-                        cache[i] |= closure(f, cache, set(f[i][arc]), arc)
-                    else:    # 对于其他非epsilon的边则向前移动一步
-                        cache[i] = set(f[i][arc])
-        # 得到闭包后的缓存
-        res |= cache[i]
-    return res
+        closure_flag[i] = False  # 初始状态设置为False
+    # 调用闭包操作函数进行递归求解
+    return opt_closure(f, closure_flag)
 
-def setDfa(E):
-    """设置DFA M的数据结构,M,N的输入字母表是相同的，即是E"""
-    dfa = {}
-    dfa["k"] = []
-    dfa["e"] = list(E)
-    dfa["f"] = {}
-    dfa["s"] = []
-    dfa["z"] = []
-    return dfa
+def opt_closure(f, closure_flag):
+    """被ε-closure(I)函数进行调用，进行递归求解，知道集合不再增大就返回调用函数"""
+    for i in list(closure_flag.keys()):
+        if "#" in f[i].keys() and closure_flag[i] == False:
+            for new_state in f[i]["#"]:
+                closure_flag[new_state] = False  # 添加新的状态，并将操作标志置为False
+            # 更改当前的标志位
+            closure_flag[i] = True
+            # 对新加入的状态进行递归求解
+            opt_closure(f,closure_flag)
+            # 没有新的状态加入就返回
+    return set(closure_flag.keys())
+
+def move(f,I,arc):
+    """move函数的实现，从集合I中的某个状态出发，经过一条arc弧到达的状态"""
+    new_states = set()    # 将转移状态集合初始化为空集
+    for i in I:
+        if arc in f[i].keys():
+            for new_state in f[i][arc]:
+                new_states.add(new_state)
+    return new_states
+
+
+
 
 if __name__ == '__main__':
     nfa = 'nfa.json'
-    K, E, f, S, Z = readNfa(nfa) # NFA M = (K, E, f, S, Z)
-    cache = set_cache(E)
-    print(f)
-    print(epsilon_closure(f,cache,S))
+    K, E, f, S, Z = readNfa(nfa) # NFA M = (K, E, f, S, Z),f['i']['arc']表示状态i经过arc弧到达的状态
+    
 
 
 
